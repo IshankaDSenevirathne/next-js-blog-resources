b610b1a2300546342f6ef528d4d7d67efb04767f
Ishanka D Senevirathne <ishankadsenevirathne@gmail.com>
2022-10-27 21:07:03 +0530

    adding types

--- pages/view-log.tsx
+++ pages/view-log.tsx
@@ -1,18 +1,36 @@
-import { NextPage } from 'next';
+import { GetServerSideProps, NextPage } from 'next';
 import { redis } from '@/lib/redis';
-const ViewLog: NextPage = ({ entries }) => {
+import React from 'react';
+
+type Entry = {
+  time: string;
+  url: string;
+  ip: string;
+  geo: object;
+};
+
+type PageProps = {
+  entries: Entry[];
+};
+
+const ViewLog: NextPage<PageProps> = ({ entries }) => {
   return (
     <div>
       <h1>Request Log</h1>
+      <ul>
+        {entries.map((entry, idx) => (
+          <li key={idx}>{JSON.stringify(entry)}</li>
+        ))}
+      </ul>
     </div>
   );
 };
 
-export async function getServerSideProps() {
-  const entries = await redis.lrange('api-request-log', 0, -1);
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const entries: Entry[] = await redis.lrange('api-request-log', 0, -1);
   return {
     props: { entries },
   };
-}
+};
 
 export default ViewLog;
